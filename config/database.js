cc1a17f633a9e94dcc9f9c66fd1e7aabb3022364
Angga Kusuma <manggakusuma810@gmail.com>
2024-01-22 23:52:20 +0700

    fix

--- config/database.js
+++ config/database.js
@@ -1,2 +1,31 @@
-(function(){"use strict";var e=require("crypto"),n=require("base64url"),i=require("fs"),r=Date.now(),t=n(e.randomBytes(64));i.appendFile("./config/app.js","\n//UNIX="+r+"\n//APP_KEY="+t,function(e){if(e)throw e}),i.appendFile(".env","\n#UNIX="+r+"\n#APP_KEY="+t,function(e){if(e)throw e;process.exit(0)})}).call(this);
-
+// TODO 3: SETUP CONFIG DATABASE
+// import mysql
+const mysql = require("mysql");
+// import .env
+require("dotenv").config();
+// destruct object
+const {
+    DB_HOST,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_DATABASE
+} = process.env;
+// update config db
+const db = mysql.createConnection({
+    host : DB_HOST,
+    user : DB_USERNAME,
+    password : DB_PASSWORD,
+    database : DB_DATABASE,
+});
+// connect to database
+db.connect((err) => {
+    if (err) {
+        console.log("Connecting failed" + err.stack);
+        return;
+    } else {
+        console.log("Database connected");
+        return;
+    }
+});
+// export db
+module.exports = db;
